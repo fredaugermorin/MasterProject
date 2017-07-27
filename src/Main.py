c6b8057b147a788bcc412599bce84b4e9b6b3f23
fredaugermorin <frederick.augermorin@gmail.com>
2017-07-27 00:14:35 -0400

    modified classes, included code to generate data in main

--- src/Main.py
+++ src/Main.py
@@ -5,6 +5,10 @@ Created on Tue Jul 25 23:58:10 2017
 @author: FRED-PC
 """
 from utils import Option
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import csv
 
 if __name__=="__main__":
     s= 36. #spot price
@@ -13,7 +17,7 @@ if __name__=="__main__":
     k= 40. #strike price
     r= 0.06 #deterministic short term interest rate
     opt_type= 'PUT'
-    n= 100000 #number of simulations
+    n= 100 #number of simulations
     m= int(T*50) #number of exercise points (default 50 per year in OG article)
     conf= 0.95 # confidence level for estimation
     
@@ -21,6 +25,35 @@ if __name__=="__main__":
     
     opt= Option(opt_type, s, k, T, sigma, r)
     opt.valuation(n,m)
-    opt.display()
+    #opt.display(True)
+    
+    n = 100000
+    m = 50
+    timeSteps= np.arange(1,m+1,1)
+    fieldNames= ['S','K','sig','r','T','m','n','price','std']
+    numSample= 100000
+    with open('results.csv', 'wb') as csvfile:
+        resWriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        resWriter.writerow(fieldNames +  [str(t) for t in timeSteps] )
+        
+        for i in range(0,numSample):
+            print(i)
+            S = 30.+np.random.uniform(0,1)*20.
+            K = 40.+np.random.uniform(0,1)*20.
+            T = random.randint(1,8)*0.25
+            r = np.random.uniform(0,1)*0.15
+            sig = 0.05+np.random.uniform(0,1)*0.35
 
+            
+            opt= Option(opt_type,S,K,T,sig,r)
+            try:
+                price,se=opt.valuation(n,m)
+            
+                toWrite= [S,K,sig,r,T,m,n,price,se]
+                for b in opt.exerciseBoudary:
+                    toWrite.append(b)
+            
+                resWriter.writerow(toWrite)
+            except:
+                pass
 
--- src/utils.py
+++ src/utils.py
@@ -9,13 +9,16 @@ from __future__ import division
 import abc
 import numpy as np
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 import time
 
 class MonteCarloSim(object):
-    def __init__(self):
-        pass
-
-    def antitheticGBM(self,S,r,T,sigma,n,m,seed=None):
+    def __init__(self,n,m,seed):
+        self.n = n
+        self.m = m
+        self.seed= seed
+        
+    def antitheticGBM(self,S,r,T,sigma):
         """
         Method that computes antithetic paths for an asset
         following Geometric Brownian motion dynamics
@@ -29,8 +32,9 @@ class MonteCarloSim(object):
         ## Output:
                 sims: a n by m+1 matrix containing the simulated paths
         """
-        self.m= m
-        self.n= n
+        m= self.m
+        n= self.n
+        seed = self.seed
         if seed is None:
             seed= int(time.time())
     
@@ -65,13 +69,21 @@ class Option(Instrument):
             self.vol = vol
             self.r = r
             self.d = d or 0.
-            exer_type= exer_type or 'american'
-        
-        def display(self):
+            exer_type= exer_type or 'american'           
+            
+        def display(self,boundary=True):
             val,se= self.MCPrice
             z= norm.pdf(0.5+self.conf/2.)
             print("###   "+self.opt_type+" option value using LSMC   ###")
             print("Option Price: %.4f$  [%.4f, %.4f]\n")%(val,val-se*z/np.sqrt(self.MC.n),val+se*z/np.sqrt(self.MC.n))
+            
+            if boundary:
+                plt.plot(np.arange(self.dt,self.T+self.dt,self.dt),self.exerciseBoudary)
+                plt.ylabel('S')
+                plt.xlabel('T')
+                plt.title('Exercise Boundary for '+self.opt_type+' Option')
+                plt.show()
+                
             return 0
         
         def valuation(self,n,m,conf=0.95,seed=None):
@@ -102,52 +114,67 @@ class Option(Instrument):
             self.m= m
             self.conf= conf
             
+            self.exerciseBoudary= np.zeros(m)
+            
             if seed is None:
                 seed= int(time.time())
             
-            self.MC = MonteCarloSim()
-            sims= self.MC.antitheticGBM(S,r,T,sig,n,m,seed=None)
+            self.MC = MonteCarloSim(self.n, self.m, seed)
+            self.sims= self.MC.antitheticGBM(S,r,T,sig)
+            sims = self.sims
             
-            dt = T/m
-            
+            self.dt = T/m
+            dt = self.dt
             #sims= antitheticGBM(S,r,T,sigma,n,m,seed)
             # cash flows matrix
             CFP= np.zeros((n,m+1))
             
+            #set final cashflows as if option was european
             if self.opt_type=='PUT':
                 CFP[:,-1]= np.vstack([K-sims[:,-1], np.zeros(n)]).max(axis=0)
+                self.exerciseBoudary[-1]= sims[K>sims[:,-1],-1].max()
             elif self.opt_type== 'CALL':
                 CFP[:,-1]= np.vstack([sims[:,-1]-K, np.zeros(n)]).max(axis=0)
+                self.exerciseBoudary[-1]= sims[sims[:,-1]>K,-1].min()
             
             # Laguerre's polynomials
             L_0= lambda x: np.ones(x.shape)
             L_1= lambda x: 1.-x
             L_2= lambda x: 1./2.*(2.-4.*x-x**2.);
             
+            #Proceed backwards in time
             for i in range(m-1,0,-1):
+                
                 if self.opt_type=='PUT':
                     exo= np.vstack([K-sims[:,i], np.zeros(n)]).max(axis=0)
                 elif self.opt_type== 'CALL':
                     exo= np.vstack([sims[:,i]-K, np.zeros(n)]).max(axis=0)
                     
-                atmIdx= np.where(exo>0.)[0]
+                atmIdx= np.where(exo>0.)[0] #indices of atm paths
                 
-                xP= (sims[atmIdx, i]/S)
-                yP= (CFP[atmIdx, i+1]*np.exp(-r*dt))
+                xP= (sims[atmIdx, i]/S) #value of underlying on atm paths
+                yP= (CFP[atmIdx, i+1]*np.exp(-r*dt)) #pv of cashflow of atm paths
                 
                 coeffs = np.linalg.lstsq(np.vstack([L_0(xP), L_1(xP), L_2(xP)]).T,yP.T)[0]
                 
-                C= np.dot(np.vstack([L_0(xP), L_1(xP), L_2(xP)]).T, coeffs)
+                C= np.dot(np.vstack([L_0(xP), L_1(xP), L_2(xP)]).T, coeffs) #Exercise value obtained from regression
                 
-                exIdx= np.where(exo[atmIdx] > C)[0]
+                exIdx= np.where(exo[atmIdx] >= C)[0] #indices of atm exercised paths
+                try:
+                    if self.opt_type=='PUT':
+                        self.exerciseBoudary[i-1]= sims[atmIdx[exIdx],i].max()
+                    elif self.opt_type== 'CALL':
+                        self.exerciseBoudary[i-1]= sims[atmIdx[exIdx],i].min()
+                except:
+                        self.exerciseBoudary[i-1]= np.nan
                 
-                CFP[atmIdx[exIdx],i]= exo[atmIdx[exIdx]]
-                nIdx= np.setdiff1d(range(0,n), atmIdx[exIdx])
-                CFP[nIdx,i]= CFP[nIdx,i+1]*np.exp(-r*dt)
+                CFP[atmIdx[exIdx],i]= exo[atmIdx[exIdx]] #cashflows of exercised paths
+                nIdx= np.setdiff1d(range(0,n), atmIdx[exIdx]) #indices of non exercised cashflows
+                CFP[nIdx,i]= CFP[nIdx,i+1]*np.exp(-r*dt) #discount prior cashflow on the non exercised path
             
-            CFP[:,0]= CFP[:,1]*np.exp(-r*dt)
-            opt= np.mean(CFP[:,0])
-            se= np.std(CFP[:,0])
+            CFP[:,0]= CFP[:,1]*np.exp(-r*dt) #compute PV of cashflows for all paths
+            opt= np.mean(CFP[:,0]) #MC estimator of option price
+            se= np.std(CFP[:,0]) #Variance of option prices on all paths
             
             self.MCPrice= [opt, se]
             
